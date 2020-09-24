c9b15e1ebb360df4f17b05dfe0d3711d7dd71a76
DANIIL BORISEVICH <71766321+PHYSI6@users.noreply.github.com>
2020-09-24 14:14:49 +0300

    Update LABA2.cpp

--- LABA2.cpp
+++ LABA2.cpp
@@ -1,36 +1,86 @@
 #include <iostream>
 #include <iomanip>
-#include <math.h>
+#include <string>
+using namespace std;
+class employee
+{
+	int nomer_sotrudnika;
+	float velichina_oklada;
+	enum etype
+	{
+		Программист = 1,Тестировщик,Аналитик,Менеджер,Начальник
+	};
+	string dolzhnost;
+	class date
+	{
+		public:
+			int den_priema;
+			int mesyac_priema;
+			int god_priema;
+	}dataprim;
+public:
+	void getempoly();
+	void putempoly();
+};
+void employee::getempoly()
+{
+	int k;
+	cout << "Введите номер сотрудника:";
+	cin >> nomer_sotrudnika;
+	cout << "Введите величину оклада (в рублях):";
+	cin >> velichina_oklada;
+	cout << "Введите день приема на работу:";
+	cin >> dataprim.den_priema;
+	cout << "Введите месяц приема на работу:";
+	cin >> dataprim.mesyac_priema;
+	cout << "Введите год приема на работу:";
+	cin >> dataprim.god_priema;
+	cout << "----Выберите профессию-----";
+	cout << "\n1.Программист\n2.Тестировщик\n3.Аналитик\n4.Менеджер\n5.Начальник отдела\n";
+	cin >> k;
+	switch (k)
+	{
+	case(Программист):
+		dolzhnost = "Программист";
+		break;
+	case(Тестировщик):
+		dolzhnost = "Тестировщик";
+		break;
+	case(Аналитик):
+		dolzhnost = "Аналитик";
+		break;
+	case(Менеджер):
+		dolzhnost = "Менеджер";
+			break;
+	case(Начальник):
+		dolzhnost = "Начальник отдела";
+				break;
+	}
+}
+void employee::putempoly()
+{
+	cout << endl;
+	cout << "\nНомер сотрудника:" << nomer_sotrudnika << "\nВеличина оклада:" << velichina_oklada << "\nДолжность:" << dolzhnost<<"\nДата приёма на работу: "<<dataprim.den_priema<<" "<<dataprim.mesyac_priema<<" "<<dataprim.god_priema;
+	cout << endl;
+}
 using namespace std;
 int main()
 {
 	setlocale(0, "rus");
-	int a, b, c;
-	double x1, x2;
-	cout << "Квадратное уравнение типа Ax^2 + Bx + C = 0" << endl;
-	cout << "Введите A:";
-	cin >> a;
-	cout << "Введите B:";
-	cin >> b;
-	cout << "Введите C:";
-	cin >> c;
-	if (b * b - 4 * a * c > 0)
+	int size;
+	employee *mas;
+	cout << "Введите количество сотрудников:";
+	cin >> size;
+	mas = new employee[size];
+	for (int i = 0; i < size; i++)
 	{
-		cout << "----Корни уравнения----" << endl;
-		x1 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-		x2 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
-		cout << "Первый корень = " << setw(10) << setfill('#') << setprecision(4) << x1 << endl;
-		cout << "Второй корень = " << setw(10) << setfill('#') << setprecision(4) << x2 << endl;
+		mas[i].getempoly();
 	}
-	else if (b * b - 4 * a * c == 0)
+	for (int i = 0; i < size; i++)
 	{
-		x1 = -b / (2 * a);
-		cout << "Единственный корень = " << setw(10) << setfill('#') << setprecision(4) << x1 << endl;
+		mas[i].putempoly();
 	}
-	else
-	{
-		cout << "Корней нет!";
-	}
+	delete[]mas;
 	system("pause");
 	return 0;
 }
