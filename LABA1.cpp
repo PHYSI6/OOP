a77c38780b1fc97f52789d14c36f165b99294d00
DANIIL BORISEVICH <71766321+PHYSI6@users.noreply.github.com>
2020-09-25 12:10:15 +0300

    Update LABA1.cpp

--- LABA1.cpp
+++ LABA1.cpp
@@ -5,8 +5,7 @@ using namespace std;
 int main()
 {
 	setlocale(0, "rus");
-	int a, b, c;
-	double x1, x2;
+	double a, b, c, x1, x2, D;
 	cout << "Квадратное уравнение типа Ax^2 + Bx + C = 0" << endl;
 	cout << "Введите A:";
 	cin >> a;
@@ -14,15 +13,16 @@ int main()
 	cin >> b;
 	cout << "Введите C:";
 	cin >> c;
-	if (b * b - 4 * a * c > 0)
+	D = b * b - 4 * a * c;
+	if (D > 0)
 	{
 		cout << "----Корни уравнения----" << endl;
-		x1 = (-b - sqrt(b * b - 4 * a * c)) / (2 * a);
-		x2 = (-b + sqrt(b * b - 4 * a * c)) / (2 * a);
+		x1 = (-b - sqrt(D)) / (2 * a);
+		x2 = (-b + sqrt(D)) / (2 * a);
 		cout << "Первый корень = " << setw(10) << setfill('#') << setprecision(4) << x1 << endl;
 		cout << "Второй корень = " << setw(10) << setfill('#') << setprecision(4) << x2 << endl;
 	}
-	else if (b * b - 4 * a * c == 0)
+	else if (D == 0)
 	{
 		x1 = -b / (2 * a);
 		cout << "Единственный корень = " << setw(10) << setfill('#') << setprecision(4) << x1 << endl;
